8f1279005dabef9fa55d373c16584795494bab89
ryuichi-maeda <uryu44213@gmail.com>
2022-09-19 12:03:41 +0900

    composer update追加

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ cd src
 git pull origin main
 
 # Install composer dependencies
+composer update
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
 
 # Clear the old cache
