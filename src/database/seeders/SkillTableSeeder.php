d8e5c11294785799d7d0132996c978532725ade6
SunamotoMikiya <miki38gtr@gmail.com>
2022-09-21 19:01:28 +0900

    skillSeeder作成

--- src/database/seeders/SkillTableSeeder.php
+++ src/database/seeders/SkillTableSeeder.php
@@ -36,6 +36,38 @@ class SkillTableSeeder extends Seeder
         Skill::create([
             'type' => SkillType::SKILL_LANGUAGE,
             'name' => 'python',
+            'weight' => 5,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'swift',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'JavaScript',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'CSS',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'C#',
             'weight' => 3,
             'icon_url' => '',
             'created_at' => Carbon::now(),
@@ -44,6 +76,166 @@ class SkillTableSeeder extends Seeder
         Skill::create([
             'type' => SkillType::SKILL_LANGUAGE,
             'name' => 'swift',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'C++',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'TypeScript',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'Ruby',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'C',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'HTML',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'SASS',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'Go',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'Kotlin',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'Rust',
+            'weight' => 1,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_LANGUAGE,
+            'name' => 'Dart',
+            'weight' => 1,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Django ',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Next.js',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Flask ',
+            'weight' => 5,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'React',
+            'weight' => 5,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Django ',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'laravel',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Node.js ',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Next.js',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Nuxt.js ',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Vue',
             'weight' => 2,
             'icon_url' => '',
             'created_at' => Carbon::now(),
@@ -51,7 +243,7 @@ class SkillTableSeeder extends Seeder
         ]);
         Skill::create([
             'type' => SkillType::SKILL_FRAMEWORK,
-            'name' => 'Django',
+            'name' => 'Angular ',
             'weight' => 2,
             'icon_url' => '',
             'created_at' => Carbon::now(),
@@ -59,7 +251,15 @@ class SkillTableSeeder extends Seeder
         ]);
         Skill::create([
             'type' => SkillType::SKILL_FRAMEWORK,
-            'name' => 'Laravel',
+            'name' => 'Rails',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_FRAMEWORK,
+            'name' => 'Flutter ',
             'weight' => 2,
             'icon_url' => '',
             'created_at' => Carbon::now(),
@@ -68,7 +268,23 @@ class SkillTableSeeder extends Seeder
         Skill::create([
             'type' => SkillType::SKILL_DATABASE,
             'name' => 'MySQL',
+            'weight' => 5,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_DATABASE,
+            'name' => 'PostgreSQL',
             'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_DATABASE,
+            'name' => 'SQLite',
+            'weight' => 1,
             'icon_url' => '',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
@@ -82,8 +298,24 @@ class SkillTableSeeder extends Seeder
             'updated_at' => Carbon::now()
         ]);
         Skill::create([
-            'type' => SkillType::SKILL_SERVICE,
-            'name' => 'AWS',
+            'type' => SkillType::SKILL_DATABASE,
+            'name' => 'Redis',
+            'weight' => 2,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_DATABASE,
+            'name' => 'FireBase',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_DATABASE,
+            'name' => 'DynamoDB',
             'weight' => 2,
             'icon_url' => '',
             'created_at' => Carbon::now(),
@@ -91,7 +323,55 @@ class SkillTableSeeder extends Seeder
         ]);
         Skill::create([
             'type' => SkillType::SKILL_SERVICE,
+            'name' => 'Heroku',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
+            'name' => 'FireBase',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
+            'name' => 'Docker',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
             'name' => 'Azure',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
+            'name' => 'AWS',
+            'weight' => 4,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
+            'name' => 'Figma',
+            'weight' => 3,
+            'icon_url' => '',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now()
+        ]);
+        Skill::create([
+            'type' => SkillType::SKILL_SERVICE,
+            'name' => 'GCP',
             'weight' => 2,
             'icon_url' => '',
             'created_at' => Carbon::now(),
